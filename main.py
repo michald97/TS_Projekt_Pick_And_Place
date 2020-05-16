49dc5e0197e47977694a1bd54d80e7117cdf5cb9
rafal9711 <rafal.biernacki@onet.pl>
2020-05-17 00:51:30 +0200

    Add new files

--- main.py
+++ main.py
@@ -5,6 +5,25 @@ from automaty.robot import *
 from automaty.master import *
 from klasy.generator import *
 from grafy.graph import *
+from symulacja.Simulation import *
+
+import argparse
+import robopy.base.model as robot
+import sys
+
+parser = argparse.ArgumentParser()
+parser.add_argument("model", help="Model of robot (puma / orion)")
+args = parser.parse_args()
+
+sim = []
+
+if str(args.model).lower() == "puma":
+    model = robot.Puma560()
+elif str(args.model).lower() == "orion":
+    model = robot.Orion5()
+else:
+    print("Bad model specified. Try again.")
+    sys.exit()
 
 
 def statemachine():
@@ -18,8 +37,62 @@ def statemachine():
 
     packing_path = ["p_0_1", "p_1_2", "p_2_3", "p_3_6", "p_6_6", "p_6_7", "p_7_1", "p_1_2", "p_2_3", "p_3_4", "p_4_5",
                     "p_5_0"]
-    robot_path = ["r_0_1", "r_1_2", "r_2_3", "r_3_4", "r_4_5", "r_5_6"]
+    robot_path = ["r_0_1", "r_1_2", "r_2_3", "r_3_4", "r_4_5", "r_5_6","r_6_0"]
     line_path = ["l_0_1", "l_1_2", "l_2_3", "l_3_4"]
+
+    def run_robot():
+        robot = Generator.create_master(robot_states, robot_transitions)
+        print("Executing path: {}".format(robot_path))
+        steps = []
+        for i, robot_event in enumerate(robot_path):
+            if robot_event == "r_0_1":
+                step = position(0, steps)
+                steps.append(step)
+                robot_transitions[robot_event]._run(robot)
+                print(robot.current_state)
+            if robot_event == "r_1_2":
+                step = position(1, steps)
+                steps.append(step)
+                robot_transitions[robot_event]._run(robot)
+                print(robot.current_state)
+            if robot_event == "r_2_3":
+                step = position(2, steps)
+                steps.append(step)
+                robot_transitions[robot_event]._run(robot)
+                print(robot.current_state)
+            if robot_event == "r_3_4":
+                step = position(3, steps)
+                steps.append(step)
+                robot_transitions[robot_event]._run(robot)
+                print(robot.current_state)
+            if robot_event == "r_4_5":
+                step = position(4, steps)
+                steps.append(step)
+                robot_transitions[robot_event]._run(robot)
+                print(robot.current_state)
+            if robot_event == "r_5_6":
+                step = position(5, steps)
+                steps.append(step)
+                robot_transitions[robot_event]._run(robot)
+                print(robot.current_state)
+            if robot_event == "r_6_0":
+                step = position(6, steps)
+                steps.append(step)
+                robot_transitions[robot_event]._run(robot)
+                print(robot.current_state)
+            if robot_event == "r_1_7" or robot_event == "r_2_7" or robot_event == "r_3_7" \
+                    or robot_event == "r_4_7" or robot_event == "r_5_7":
+                step = position(7, steps)
+                steps.append(step)
+                robot_transitions[robot_event]._run(robot)
+                print(robot.current_state)
+            if robot_event == "r_7_8":
+                step = position(8, steps)
+                steps.append(step)
+                robot_transitions[robot_event]._run(robot)
+                print(robot.current_state)
+        run(steps, model) #run animation
+
 
     # execute paths
     for path in paths:
@@ -38,68 +111,46 @@ def statemachine():
 
             # add slave
             if supervisor.current_state.value == "packing":
-                # TODO: automata 1 (for) slave1
                 packing = Generator.create_master(packing_states, packing_transitions)
-                # print('\n' + str(packing))
                 print("Executing path: {}".format(packing_path))
+
                 for packing_event in packing_path:
                     packing_transitions[packing_event]._run(packing)
                     print(packing.current_state)
                     if (packing.current_state.value) == "p1":
-                        robot = Generator.create_master(robot_states, robot_transitions)
-                        print("Executing path: {}".format(robot_path))
-                        for robot_event in robot_path:
-                            robot_transitions[robot_event]._run(robot)
-                            print(robot.current_state)
+                        run_robot()
+
                     if (packing.current_state.value) == "p3":
-                        robot = Generator.create_master(robot_states, robot_transitions)
-                        print("Executing path: {}".format(robot_path))
-                        for robot_event in robot_path:
-                            robot_transitions[robot_event]._run(robot)
-                            print(robot.current_state)
+                        run_robot()
 
             if supervisor.current_state.value == "line_1":
-                # TODO: automata 2 (for) slave2
                 line_1 = Generator.create_master(line_1_states, line_1_transitions)
-                # print('\n' + str(packing))
                 print("Executing path: {}".format(line_1))
+
                 for line_event in line_path:
                     line_1_transitions[line_event]._run(line_1)
                     print(line_1.current_state)
                     if (line_1.current_state.value) == "q1":
-                        robot = Generator.create_master(robot_states, robot_transitions)
-                        for robot_event in robot_path:
-                            robot_transitions[robot_event]._run(robot)
-                            print(robot.current_state)
+                        run_robot()
 
             if supervisor.current_state.value == "line_2":
-                # TODO: automata 3 (for) slave3
                 line_2 = Generator.create_master(line_2_states, line_2_transitions)
-                # print('\n' + str(packing))
                 print("Executing path: {}".format(line_2))
                 for line_event in line_path:
                     line_2_transitions[line_event]._run(line_2)
                     print(line_2.current_state)
                     if (line_2.current_state.value) == "q1":
-                        robot = Generator.create_master(robot_states, robot_transitions)
-                        for robot_event in robot_path:
-                            robot_transitions[robot_event]._run(robot)
-                            print(robot.current_state)
+                        run_robot()
 
             if supervisor.current_state.value == "stop":
-                # TODO: automata 5 (for) slave3
-                ...
                 print("Supervisor done!")
             if supervisor.current_state.value == "error":
-                # TODO: automata 6 (for) slave3
-                ...
                 print("Error!")
 
 
 
 if __name__ == "__main__":
-    statemachine()
-    print('\n')
     search()
     display()
-
+    print('\n')
+    statemachine()
---
+++ symulacja/Simulation.py
@@ -0,0 +1,77 @@
+import numpy as np
+
+from commands.moves import  move_j
+
+def position(number, steps):
+
+    if number == 0:
+
+        print("q_0 = Oczekiwanie na sygnał startu z sterownika")
+        initialize = [ 90.0, 90.0, 0.0, 0.0, 0.0, 0.0 ]
+        return initialize
+
+    if number == 1:
+        print("q_1 = Uruchomienie robota")
+        start = [ 90.0, 90.0, 0.0, 0.0, 0.0, 0.0 ]
+        return start
+
+    if number == 2:
+        print("q_2 = Podjechanie końcówki robota do obiektu")
+        To_Object = [ 90.0, 90.0, 30.0, 0.0, 0.0, 0.0 ]
+        return To_Object
+
+    if number == 3:
+        print("q_3 = Uruchomienie ssawki")
+        Catch_Object = [ 90.0, 150.0, 30.0, 0.0, 0.0, 0.0 ]
+        return Catch_Object
+
+    if number == 4:
+        print("q_4 = Przniesienie obiektu na nowe miejsce")
+        Move_Object = [ 90.0, 90.0, 30.0, 0.0, 45.0, 0.0 ]
+        return Move_Object
+
+    if number == 5:
+        print("q_5 = Wyłącznie ssawki")
+        Put_Object = [ 0.0, 135.0, 0.0, 0.0, 45.0, 0.0 ]
+        return Put_Object
+
+    if number == 6:
+        print("q_6 =  Powrót robota do pozycji początkowej")
+        To_Start_Position = [ 0.0, 90.0, 30.0, 0.0, 45.0, 0.0 ]
+        return To_Start_Position
+
+    if number == 7:
+        print("q_7 =  Wysłanie sygnału o błędzie do sterownika")
+        error = steps[len(steps)-1]
+        return error
+
+    if number == 8:
+        print("q_8 =  Przejście w tryb oczekiwania na wciśnięcie przycisku restart")
+        wait_mode = steps[len(steps)-1]
+        return wait_mode
+
+def run(steps,robot):
+
+    moves=[]
+
+    for i in range(len(steps)):
+        if i == len(steps) - 1:
+            move = move_j(robot, steps[len(steps) - 1], steps[0])
+        else:
+            move = move_j(robot, steps[i], steps[i + 1])
+        moves.append(move)
+
+    Path = moves[0]
+
+    for i in range(len(moves)-1):
+        Path = np.concatenate((Path, moves[i+1]), axis=0)
+
+    # animate robot
+    robot.animate(stances=Path, frame_rate=30, unit='deg')
+
+
+
+
+
+
+
